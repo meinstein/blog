adbe891e351275c6954694d025de3b361cd56a80
Max <maximillianeinstein@gmail.com>
2019-01-21 20:57:36 -0800

    fixed styling

--- metadata.js
+++ metadata.js
@@ -1,7 +1,7 @@
 export default [
   {
     date: '1.28.2017',
-    title: 'The Most Meaningful Moment of 1.21.2017',
+    title: 'The Most Meaningful Moment',
     snippet:
       'She displayed a remarkable sense of urgency that stood in stark contrast to the flurry of activity happening just blocks away.',
     route: '/meaningful-moment',
